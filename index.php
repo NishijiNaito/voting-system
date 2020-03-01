eccdeb20077625109139dd3f44f40b9ff61ea4d8
summakarn <summakarn@gmail.com>
2020-03-01 20:18:58 +0700

    [Add] Void Index

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Cs Project Storage System</title>
+    <link rel="stylesheet" href="./asset/css/bootstrap.min.css">
+
+
+</head>
+
+<body>
+
+    ิ
+
+    <div class="container ">
+        <div class="row justify-content-center">
+            <div class="col-sm-11">
+                <div class="card">
+                    <div class="card-header">
+                        Menu List
+                    </div>
+                    <div class="card-body">
+                        <div class="row justify-content-center">
+                            <div class="col-sm-11">
+                                <div class="card">
+                                    <div class="card-body">
+                                        
+                                        <a class="card-text" href="view-vote">View Vote</a>
+
+
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="card">
+                                    <div class="card-body">
+                                        
+                                        <a class="card-text" href="redeem-vote">Redeem Vote</a>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <script src="./asset/js/jquery-3.4.1.slim.min.js"></script>
+    <script src="./asset/js/popper.min.js"></script>
+    <script src="./asset/js/bootstrap.min.js"></script>
+</body>
